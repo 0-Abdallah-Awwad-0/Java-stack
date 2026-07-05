b207f17f826cabedaabf2e6bd6dfb654a908da84
0-Abdallah-Awwad-0 <abdallah22awwad11@gmail.com>
2026-07-05 23:10:45 +0300

    OOP

---
+++ JavaOOP/ZooKeeper/src/Zookeeper/Mammal.java
@@ -0,0 +1,14 @@
+package Zookeeper;
+
+public class Mammal {
+	//Mammal VAriable
+	protected int energy;
+	//Constructor
+	public Mammal(){
+		energy = 100;
+	}
+	//Display Function
+	public void Display() {
+		System.out.println("Energy Level :" + energy);
+	}
+}
